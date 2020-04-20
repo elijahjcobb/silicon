c2aa024843f7e69df9483f080e8446827919cb42
Elijah Cobb <elijah@elijahcobb.com>
2020-04-20 10:59:38 -0400

    tests

--- ts/tests/index.test.ts
+++ ts/tests/index.test.ts
@@ -1,6 +1,34 @@
-describe("group", (): void => {
+beforeEach(async(): Promise<void> => {
 
-	test("test", (): void => {
+});
+
+afterEach(async (): Promise<void> => {
+
+});
+
+describe("SiObject Methods", async(): Promise<void> => {
+
+	test("test", async(): Promise<void> => {
+
+		expect(2).toEqual(2);
+
+	});
+
+});
+
+describe("SiObject Props Types", async(): Promise<void> => {
+
+	test("test", async(): Promise<void> => {
+
+		expect(2).toEqual(2);
+
+	});
+
+});
+
+describe("Query", async(): Promise<void> => {
+
+	test("test", async(): Promise<void> => {
 
 		expect(2).toEqual(2);
 
